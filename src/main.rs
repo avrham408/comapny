05f2604337de0f9aa4e6f9189adeab0d7b55d3ba
avi <avrham408@gmail.com>
2020-05-16 12:06:20 +0300

    add more commands

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,28 @@ enum CommandType {
     SHOW,
     EXIT,
     CREATE,
+    MOVE,
     UNKNOWN,
 }
 
 fn handle_messages(message: &String, company: &mut HashMap<String, Vec<String>>) {
     let commands = split_message_to_commands(message);
     match handle_command_type(&commands[0]) {
-        CommandType::ADD => {
-            handle_add_message(&commands, company);
-        }
-        CommandType::SHOW => {
-            handle_show(&commands, company);
-        }
-        CommandType::EXIT => {
-            process::exit(0);
-        }
-        CommandType::UNKNOWN => {
-            println!("command uknown - {}", message.trim());
-            println!("Usage: [OPTION][VAIRABLES]");
-        }
+        CommandType::ADD => handle_add_message(&commands, company),
+        CommandType::SHOW => handle_show(&commands, company),
+        CommandType::EXIT => process::exit(0),
+        CommandType::MOVE => handle_move(&commands, company),
+        CommandType::CREATE => handle_create(&commands, company),
+        CommandType::UNKNOWN => handle_unkown(message),
     };
+}
+
+fn handle_unkown(message: &String) -> bool {
+    println!(
+        "command uknown - {}\nUsage: [OPTION][VAIRABLES]",
+        message.trim()
+    );
+    true
 }
 
 fn split_message_to_commands(message: &String) -> Vec<String> {
@@ -41,10 +43,64 @@ fn handle_command_type(command: &str) -> CommandType {
     match command {
         "add" => return CommandType::ADD,
         "show" => return CommandType::SHOW,
-        "exit" | "close" => CommandType::EXIT,
+        "exit" | "close" | "quit" => CommandType::EXIT,
         "create" => CommandType::CREATE,
+        "move" => CommandType::MOVE,
         _ => return CommandType::UNKNOWN,
     }
+}
+
+fn handle_move(commands: &Vec<String>, company: &mut HashMap<String, Vec<String>>) -> bool {
+    if commands.len() != 4 || commands[2] != "to" {
+        println!("Failed\nUsage: move [EMPLOYEE] to [DEPARTMENT]");
+        return false;
+    }
+
+    let employee = &commands[1];
+    let department = &commands[3];
+    if !valid_department_exist(company, department) {
+        println!("Failed!{} department not exist", department);
+        return false;
+    }
+    if valid_employee_not_exist(company, employee) {
+        println!("Failed!{} employee not exist", department);
+        return false;
+    }
+
+    remove_employee_department(employee, department, company);
+    add_employee_to_department(company, department, employee);
+    return true;
+}
+
+fn remove_employee_department(
+    employee: &String,
+    department: &String,
+    company: &mut HashMap<String, Vec<String>>,
+) -> bool {
+    for (_, emp_vec) in company {
+        // search for current employee position for deletion
+        let mut pos = 0;
+        let cuurent_departmt_pos = department.len() - 1;
+        while pos <= cuurent_departmt_pos {
+            if &emp_vec[pos] == employee {
+                emp_vec.remove(pos);
+                return true;
+            }
+            pos += 1;
+        }
+    }
+    false
+}
+
+fn handle_create(commands: &Vec<String>, company: &mut HashMap<String, Vec<String>>) -> bool {
+    if commands.len() != 2 {
+        println!("Failed\nUsage: create [DEPARTMENT]");
+    } else {
+        let department = &commands[1];
+        company.insert(department.to_string(), Vec::new());
+        println!("{} is created", department);
+    }
+    true
 }
 
 fn handle_show(commands: &Vec<String>, company: &mut HashMap<String, Vec<String>>) -> bool {
@@ -52,7 +108,7 @@ fn handle_show(commands: &Vec<String>, company: &mut HashMap<String, Vec<String>
         1 => println!("{:#?}", company),
         2 => show_department(&commands[1], company),
         _ => {
-            println!("Failed\nUsage: show [DEPARTMENT] OR show");
+            println!("Failed\nUsage: show [DEPARTMENT] show");
             return false;
         }
     }
@@ -70,33 +126,43 @@ fn show_department(department: &String, company: &mut HashMap<String, Vec<String
 }
 
 fn handle_add_message(commands: &Vec<String>, company: &mut HashMap<String, Vec<String>>) -> bool {
-    let employee = &commands[1].to_lowercase();
-    let to = &commands[2].to_lowercase();
-    let department = &commands[3].to_lowercase();
-    if commands.len() != 4 || to != "to" {
+    if commands.len() != 4 || commands[2] != "to" {
         println!("Failed\nUsage: add [EMPLOYEE] to [DEPARTMENT]");
         return false;
     }
-    if valid_department_exist(company, department) == false
-        || valid_employee_exist(company, employee) == false
-    {
+    let employee = &commands[1].to_lowercase();
+    let department = &commands[3].to_lowercase();
+
+    if !valid_department_exist(company, department) {
+        println!("Failed\n{} department is not exist", department);
+        println!("to create department Usage: create [DEPARTMENT]");
+        println!("Usage: add [EMPLOYEE] to [DEPARTMENT]");
+        return false;
+    } else if !valid_employee_not_exist(company, employee) {
+        println!("Failed\n{} is in another department", employee);
+        println!("to switch department Usage: move [EMPLOYEE] to [DEPARTMENT]");
         println!("Usage: add [EMPLOYEE] to [DEPARTMENT]");
         return false;
     }
+    add_employee_to_department(company, department, employee);
+    true
+}
 
+fn add_employee_to_department(
+    company: &mut HashMap<String, Vec<String>>,
+    department: &String,
+    employee: &String,
+) {
     company
         .entry(department.to_string())
         .or_insert(Vec::new())
         .push(employee.to_string());
     println!("{} added to {} department", employee, department);
-    true
 }
 
 fn valid_department_exist(company: &HashMap<String, Vec<String>>, department: &String) -> bool {
     match company.get(department) {
         Option::None => {
-            println!("Failed\n{} department is not exist", department);
-            println!("to create department Usage: create [DEPARTMENT]");
             return false;
         }
         _ => (),
@@ -104,12 +170,10 @@ fn valid_department_exist(company: &HashMap<String, Vec<String>>, department: &S
     true
 }
 
-fn valid_employee_exist(company: &HashMap<String, Vec<String>>, employee: &String) -> bool {
-    for (department, employees) in company {
+fn valid_employee_not_exist(company: &HashMap<String, Vec<String>>, employee: &String) -> bool {
+    for (_, employees) in company {
         for exist_employee in employees {
             if exist_employee == employee {
-                println!("Failed\n{} is in {} ", employee, department);
-                println!("to switch department Usage: move [EMPLOYEE] to [DEPARTMENT]");
                 return false;
             }
         }
@@ -122,7 +186,9 @@ fn main() {
     let mut my_vec = Vec::new();
     my_vec.push("avi".to_string());
     company.insert("engineering".to_string(), my_vec);
+    println!("welcome to HR program");
     loop {
+        println!("please enter command");
         let mut message = String::new();
         io::stdin()
             .read_line(&mut message)
